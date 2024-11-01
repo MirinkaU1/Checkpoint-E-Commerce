eb15ee32ba171c9b5b36f302c52acc6b97da53a0
Mohamed Abraham CHERIF <cherifmohamedabraham@gmail.com>
2024-11-01 00:03:04 +0000

    paufinage

--- src/components/pages/UpdatePassword.js
+++ src/components/pages/UpdatePassword.js
@@ -113,32 +113,37 @@ export default function ResetPassword() {
             >
               {showNewPassword ? (
                 <svg
-                  className="h-5 w-5 text-gray-500"
-                  fill="none"
                   xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
                   viewBox="0 0 24 24"
+                  strokeWidth="1.5"
                   stroke="currentColor"
+                  className="size-6"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M13.875 18.825A10.05 10.05 0 0112 19.5c-5.385 0-9.873-3.941-11.25-9A10.054 10.054 0 014.125 5.175m4.927-2.077A9.956 9.956 0 0112 4.5c5.385 0 9.873 3.941 11.25 9a10.05 10.05 0 01-2.014 3.527M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
                   />
                 </svg>
               ) : (
                 <svg
-                  className="h-5 w-5 text-gray-500"
-                  fill="none"
                   xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
                   viewBox="0 0 24 24"
+                  strokeWidth="1.5"
                   stroke="currentColor"
+                  className="size-6"
                 >
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3l18 18M9.88 9.88A3 3 0 0115 12m0 0a3 3 0 01-4.243 4.243M15 12a3 3 0 00-3-3m0 0a3 3 0 00-4.243 4.243M9.88 9.88L5.12 5.12M15 12l4.88 4.88M3 3l18 18"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                   />
                 </svg>
               )}
